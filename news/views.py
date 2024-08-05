a3d03231a0209a704c03cbbe91c67c508b54eab4
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-08-05 12:07:29 +0200

    added news api python code in views and urls

--- news/views.py
+++ news/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .news_api import get_news
 
 # Create your views here.
 def index(request):
     return render(request, 'card/home.html')
+
+def home(request):
+    country = request.GET.get('country', 'us')
+    category = request.GET.get('category', 'general')
+    page = int(request.GET.get('page', 1))
+    
+    news = get_news(country=country, category=category, page=page)
+    articles = news.get('articles', [])
+
+    return render(request, 'card/home.html', {
+        'news': articles,
+        'country': country,
+        'category': category,
+        'page': page,
+        'has_more': len(articles) == 20 
+    })
